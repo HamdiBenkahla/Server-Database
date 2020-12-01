00210f3baa9fa37fa076f14893fe9a4bc256dd2e
BasmaBenNasr <bennasrbasma97@gmail.com>
2020-12-01 10:40:51 +0100

    adding the method that will insert a new row in the rides table

--- server/routes/rides.js
+++ server/routes/rides.js
@@ -1,37 +1,41 @@
 const express = require('express');
-const { where } = require('sequelize');
 const router= express.Router();
 const {Ride, Driver, Passenger} = require('../../database/models');
-const { Op } = require("sequelize");
+const { Op, literal } = require("sequelize");
+var twilio = require("twilio");
+
+var accountSid = "AC2a8af006e7a6678aae74e361dd5b598c"; 
+var authToken = "bfee0435ad1a900feeed5ae9f59381bb";
 
 
-// Ride.create()
-router.get('/', async(req, res) => {
-    await Ride.findAll().then((passenger) => res.json(passenger))
+router.get('/:id', async(req, res) => {
+  try {
+    const passengerId = +req.params.id;
+    const passenger = await Passenger.findByPk(passengerId);
+    const myRides = await passenger.getRides();
+    const rides = await Ride.findAll({
+      where: {checkedStatus: false},
+      include: [Driver]
+    });
+    for(var i = 0; i < rides.length; i++) {
+      for(var j = 0; j < myRides.length; j++) {
+        if(rides[i].id === myRides[j].id) {
+          rides.splice(i, 1);
+          i++;
+          break;
+        }
+      }
+    }
+    res.status(200).json(rides);
+    } catch(error) {
+        res.status(405).json(error);
+    }
 });
 
 
 router.get('/ride', async(req, res) => {
     await Ride.findOne({where: {date: req.body.date}}).then((ride) => res.json(ride))
 });
-
-
-// router.post('/', async(req, res) => {
-//     await Ride.create({
-//         departure: req.body.deparature,
-//         destination: req.body.destination,
-//         time: req.body.time,
-//         date: req.body.date,
-//         price: req.body.price,
-//         seats: req.body.seats,
-//         stop1: req.body.stop1,
-//         stop2: req.body.stop2,
-//         stop3: req.body.stop3,
-//         stop4: req.body.stop4,
-//         driverId: req.driverId
-//     })
-//     .then((ride) => res.json(ride))
-// })
 
 
 router.post("/reserve/add", async (req, res) => {
@@ -42,70 +46,72 @@ router.post("/reserve/add", async (req, res) => {
     ride.addPassenger(passengerId);
   });
 
-
-//   const ride = await Passenger.create({ id: passengerId });
-//   await user.addRide([ride,RidePassengers]);
-//   const appointment = await sequelize.query(`INSERT INTO RidePassenger(rideId,passengerId) VALUES("${rideId}","${passengerId}"`, { type: QueryTypes.INSERT});
-  
 router.post('/search', async(req, res) => {
   try {
-   const rides = await Ride.findAll({
-     where: {
-       departure: req.body.departure,
-       destination: req.body.destination
-     },
-     include: [Driver]
-   });
-   console.log(rides)
-   res.status(200).json(rides);
-   } catch(error) {
-       res.status(405).json(error);
-   }
+    const passengerId = req.body.passengerId;
+    console.log(req.body)
+    const passenger = await Passenger.findByPk(passengerId);
+    console.log(passenger)
+    const myRides = await passenger.getRides();
+    console.log(myRides)
+    const rides = await Ride.findAll({
+      where: {
+        departure: req.body.departure,
+        destination: req.body.destination,
+        checkedStatus: false
+      },
+      include: [Driver]
+    });
+    // console.log(rides);
+    const searchedRides = [];
+    for(var i = 0; i < rides.length; i++) {
+      for(var j = 0; j < myRides.length; j++) {
+        if(rides[i].id === myRides[j].id) {break;}
+      }
+      if(j === myRides.length) {searchedRides.push(rides[i]);}
+    }
+    console.log(searchedRides)
+    res.status(200).json(searchedRides);
+    } catch(error) {
+        res.status(405).json(error);
+    }
 });
 
-// router.post('/reserve', async (req, res) => {
-//   console.log(req.body)
-//   try {
-//     const passengerId = req.body.passengerId;
-//     const rideId = req.body.rideId;
-//     console.log(rideId)
-//     let ride = await Ride.findByPk(rideId);
-//     let reserved = await ride.addPassenger(passengerId);
-//     if(reserved) return res.json('reserved');
-//   } catch(error) {
-//     res.status(405).json(error);
-//   }
-// });
 
-router.post('/reserve',async(req,res)=>{
-  console.log(req.body);
-  const ride_id = req.body.rideId;
-  const passenger_id = req.body.passengerId;
+router.post('/reserve',async(req,res)=>{ 
   try{
-    const seat = await Ride.findByPk(ride_id)
-    if (seat.checkedStatus === true) return res.send({message: "no more seats for this ride"});
-    if(seat.seats !== 0){
- await Ride.decrement('seats', { where: { id: ride_id }});
- console.log(ride, "here i am")
-   const updated = await Ride.findByPk(ride_id)
-   if(updated.seats === 0){
-   await Ride.update({ checkedStatus : true })	
-     }
-   res.status(200).json('place is reserved!')
-  }
-  }catch(error){
-   res.status(405).json(error)
-  }
+    var client = new twilio(accountSid, authToken);
+    console.log(req.body);
+  const rideId = req.body.rideId;
+  const passengerId = req.body.passengerId;
+    
+  await Ride.decrement('seats', { where: { id: rideId }});
+  await Ride.update({ checkedStatus: true}, { where: { id: rideId, seats: 0 }})
+      const ride = await Ride.findByPk(rideId)
+      const passenger = await Passenger.findByPk(passengerId)
+      console.log(passenger.phoneNumber)
+      console.log(ride);
+      let reserved = await ride.addPassenger(passengerId);
+          if(reserved){ 
+            client.messages.create({
+      body: "Hello doctor this from your app",
+      to: `+ ${passenger.phoneNumber}`, // Text this number
+      from: "+19387661291", // From a valid Twilio number
+    })
+            return res.json('reserved', message);}
+        } catch(error) {
+          res.status(405).json(error);
+        }
 })
 
 
-router.get('/:id', async(req, res) => {
+router.get('/passenger/:id', async(req, res) => {
   try{
-    console.log(req.params)
+    // console.log(req.params)
     const passengerId = Number(req.params.id);
     const passenger = await Passenger.findByPk(passengerId);
-    const rides = await passenger.getRides();
-    console.log(rides)
+    const rides = await passenger.getRides({include: [Driver]});
+    // console.log('rides', rides);
         if(rides.length){
          res.status(200).json(rides);
         }
@@ -114,92 +120,69 @@ router.get('/:id', async(req, res) => {
   }
 })
 
+router.get('/passengers/:id', async(req, res) => {
+  try{
+    const rideId = Number(req.params.id);
+    const ride = await Ride.findByPk(rideId);
+    const passengers = await ride.getPassengers();
+    console.log(passengers);
+    if(passengers.length) {
+      res.status(200).json(passengers);
+    }
+  } catch(error) {
+    res.status(500).json(error);
+  }
+})
 
 //basma
 //will insert a new row in the rides table
 router.post('/create', async(req, res) => {
-    try{console.log(req.body)
-   const ride = await Ride.create({
-     
-       departure: req.body.departure,
-       destination: req.body.destination,
-       date: req.body.date,
-       time: req.body.time,
-       seats: req.body.seats,
-       price: req.body.price,
-       stop1: req.body.stop1,
-       stop2: req.body.stop2,
-       stop3: req.body.stop3,
-       stop4: req.body.stop4,
-       driverId: req.body.driverId
-       })
-       console.log(ride)
-       res.json(ride)
-    }catch(error){
-     res.status(500).json(error)
-    }
-   })
+  try{console.log(req.body)
+ const ride = await Ride.create({
+   
+     departure: req.body.departure,
+     destination: req.body.destination,
+     date: req.body.date,
+     time: req.body.time,
+     seats: req.body.seats,
+     price: req.body.price,
+     stop1: req.body.stop1,
+     stop2: req.body.stop2,
+     stop3: req.body.stop3,
+     stop4: req.body.stop4,
+     driverId: req.body.driverId
+     })
+     console.log(ride)
+     res.json(ride)
+  }catch(error){
+   res.status(500).json(error)
+  }
+ })
 
   //  1 - making an empty memory array to put the filtred data in it
   //  2 - getting all the rides from ride table by driver id
   //  3 - filter the data from database where checkedStatus is false
   //  4 - send the response to the front end in an object where the key is data
   //    find() for any field
-   router.get('/:id',async (req,res) => {  
-    const driverId = Number(req.params.id); 
-    const rides = await Ride.findAll({
-        where: {
-          [Op.and]: [
-            { driverId: driverId },
-            { checkedStatus: false }
-          ]
-        }
-      });
-        res.json(rides) 
+  router.get('/driver/:id',async (req,res) => {
+    try {
+      console.log(req.params)
+   const driverId = Number(req.params.id); 
+   const rides = await Ride.findAll({
+       where: {
+           driverId: driverId,
+           ratedStatus: false
+       }, order: [['createdAt', 'DESC']] ,
+       include: [Passenger]
+     });
+     console.log(rides)
+     if(rides.length){
+       res.status(200).json(rides);
+      }
+  }catch(error) {
+    res.status(500).json(error);
+}
 });
-
-// router.post('/reserve',async(req,res)=>{
-//   console.log(maxSeats)
-//   ride = await Ride.Update({
-//     where: {
-//       [Op.and]: [
-//         {maxSeats: req.body.maxSeats},
-//         { checkedStatus: true }
-//       ]
-//     }
-//   });
-//   res.json({maxSeats}) 
-// })
-
-
-//updating the number of seats every time a passenger reserves
-// router.post('/ride/reserve',async(req,res)=>{
-//   const seat_id = req.body.seatId;
-//   const driver_id = req.body.driverId;
-//   const passenger_id = req.body.passengerId;
-//   try{
-//     const seat = await Ride.findPk(seat_id)
-//     if(seat.seats < 5){
-//    await Ride.increment({seats: +1, where:{
-//                 [Op.and]: [
-//                   {driverId: driver_id },
-//                   { passengerId : passenger_id }
-//                 ]
-//               }
-//               });
-//    const updated = await Ride.findPk(seat_id)
-//    if(updated.seats === 4){
-//    await Ride.update({ checkStatus : true })	
-//      }
-//    res.status(200).json('place is reserved!')
-//   }
-//   }catch(error){
-//    res.status(405).json(error)
-//   }
-// })
-   
-
-  
 
 
 module.exports = router ;
--- server/server.js
+++ server/server.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fileupload = require("express-fileupload")
+const file = require("./routes/upload.js");
+const dotenv = require('dotenv');
+require('dotenv').config()
+
+app.use(fileupload({useTempFiles: true}));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
@@ -11,11 +18,14 @@ const car = require('./routes/car.js')
 const driver = require('./routes/drivers.js')
 const passenger = require('./routes/passengers.js')
 const ride = require('./routes/rides.js')
+const feedback = require('./routes/feedback.js')
 
+app.use("/file",file)
 app.use("/car", car);
 app.use("/passenger", passenger);
 app.use("/driver", driver);
 app.use("/ride", ride);
+app.use('/feedback', feedback);
 
 
 const PORT = 3000;
